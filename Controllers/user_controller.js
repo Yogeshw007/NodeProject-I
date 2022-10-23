3d08014a6be2bfd79e02ae3c415dc67191728fae
Yogeshw007 <yogilal97@gmail.com>
2022-10-23 22:27:12 +0530

    Created manual authentication for users

--- Controllers/user_controller.js
+++ Controllers/user_controller.js
@@ -2,20 +2,13 @@
 
 const User = require("../models/user");
 
-module.exports.profile = function (req, res) {
-    return res.render('user_profile', {
-        title: 'User profile'
-    });
-}
-
-
-module.exports.signup = function (req, res) {
+module.exports.signUp = function (req, res) {
     return res.render('sign_up', {
         title: 'Sign up'
     });
 }
 
-module.exports.signin = function (req, res) {
+module.exports.signIn = function (req, res) {
     return res.render('sign_in', {
         title: 'Sign in'
     });
@@ -48,6 +41,48 @@ module.exports.create = function (req, res) {
 
 }
 
-module.exports.signIn = function (req, res) {
-    //TODO later
+module.exports.createSession = function (req, res) {
+    //steps to authenticate
+    //find the user
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) { console.log('Error in finding user in signing in!') };
+
+        //handle user found
+        if (user) {
+
+            //handle password which doesn't match
+            if (user.password != req.body.password) {
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        } else {
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+}
+
+module.exports.profile = function (req, res) {
+    if (req.cookies.user_id) {
+        User.findById(req.cookies.user_id, function (err, user) {
+            if (user) {
+                return res.render('user_profile', {
+                    title: 'User profile',
+                    user: user
+                });
+            } else {
+                return res.redirect('/users/sign-in');
+            }
+        });
+    } else {
+        return res.redirect('/users/sign-in');
+    }
+}
+
+module.exports.signOut = function (req, res) {
+    res.clearCookie('user_id');
+    return res.redirect('/users/sign-in');
 }
--- Routes/users.js
+++ Routes/users.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/user_controller');
+const User = require('../models/user');
 
-router.get('/profile', usersController.profile);
-router.get('/sign-up', usersController.signup);
-router.get('/sign-in', usersController.signin);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
+router.get('/profile', usersController.profile);
+router.get('/sign-out', usersController.signOut);
 
 module.exports = router;
