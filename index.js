4bad7892c1207abc110a3f5bebceeed9ec634029
Yogeshw007 <yogilal97@gmail.com>
2022-10-23 12:01:58 +0530

    Adding styles to the view file

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+//Set the static file path - When using the assets directly give the sub folders like css/layout.css
+app.use(express.static('./assets'));
+
 // Use the layouts before the routes
 app.use(expressLayouts);
+
+//Set css & js to add in the head
+app.set('layout extractStyles', true);
+app.set('layout extractScript', true);
 
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-
 
 app.listen(port, function (err) {
     if (err) {
