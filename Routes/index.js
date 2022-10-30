603fe0381073e92015d192cd89e3797d00849118
Yogeshw007 <yogilal97@gmail.com>
2022-10-30 13:59:26 +0530

    Adding posts routes, controller and view

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-// const usersController = require('../Controllers/user_controller');
+// const usersController = require('../controllers/user_controller');
 
 router.get('/', homeController.home);
 
 //Path that starts with /users should
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 module.exports = router;
