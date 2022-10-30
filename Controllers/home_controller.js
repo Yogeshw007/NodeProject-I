76941781bb0c81888d5a9b466a0cdbbaed782d42
Yogeshw007 <yogilal97@gmail.com>
2022-10-30 15:33:44 +0530

    Updated the action to prepopulate and added in views

--- Controllers/home_controller.js
+++ Controllers/home_controller.js
@@ -1,5 +1,35 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
-    return res.render('home', {
-        title: 'Home',
+    let allPosts = new Array();
+
+    // Find all the posts from the db and send the posts object in the response
+    // Post.find({}, function (err, posts) {
+    //     if (err) {
+    //         console.log('Error in finding the posts');
+    //         return res.render('home', {
+    //             title: 'Home'
+    //         });
+    //     }
+
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         posts: posts
+    //     });
+    // });
+
+    // Find all the posts and populate the user (retrieve the entire user object) and send the posts object in the response
+    Post.find({}).populate('user').exec(function (err, posts) {
+        if (err) {
+            console.log('Error in finding the posts');
+            return res.render('home', {
+                title: 'Home'
+            });
+        }
+
+        return res.render('home', {
+            title: 'Home',
+            posts: posts
+        });
     });
 }
--- Controllers/posts_controller.js
+++ Controllers/posts_controller.js
@@ -2,8 +2,6 @@ const Post = require('../models/post');
 
 module.exports.create = function (req, res) {
 
-    console.log(req.user);
-
     Post.create({
         content: req.body.content,
         user: req.user._id
