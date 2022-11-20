176ceea5ff13c18e32a7b03f66c891485d3a598d
Yogeshw007 <yogilal97@gmail.com>
2022-11-20 17:14:17 +0530

    Adding dynmaic into the email

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,13 +1,13 @@
 const nodemailer = require('../config/nodemailer');
 
 exports.newComment = function (comment) {
-    console.log('inside new comment mailer');
+    let htmlString = nodemailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
 
     nodemailer.transporter.sendMail({
         from: 'yogeshbala908@gmail.com',
         to: comment.user.email,
         subject: 'New comment Published!',
-        html: '<h1>Yup your comment is now published.</h1>'
+        html: htmlString
     }, (err, info) => {
         if (err) {
             console.log('Error in sending email', err);
